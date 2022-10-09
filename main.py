ec41f7bc5ff098d7f1ffad1c85f958d42c03fd23
Ma-Ko-dev <Mathias.Kowitz@gmx.de>
2022-10-09 15:46:28 +0200

    added working buttons and random sfx

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from tkinter import *
 from gui_button import GuiButton
 from playsound import playsound
@@ -8,25 +9,45 @@ BLUE1 = "#F9F7F7"
 BLUE2 = "#DBE2EF"
 BLUE3 = "#3F72AF"
 BLUE4 = "#112D4E"
-SFX = "sfx/sheep_noise.mp3"
+SHEEP_SFX = "sfx/sheep_noise.mp3"
+YAWN_SFX = ["sfx/yawn1.mp3", "sfx/yawn2.mp3", "sfx/yawn3.mp3"]
 
 timer = None
 move_stop = False
+sheep_count = 1
+user_count = ""
 
 
-def hallo_welt():
-    # just for testing :D
-    print("Hello World")
+def btn_send() -> None:
+    global sheep_count
+    global user_count
+    if int(user_count) == sheep_count:
+        sheep_count += 1
+        move_image()
+    else:
+        sheep_count = 1
+    btn_clear()
+
+
+def btn_clear() -> None:
+    global user_count
+    user_count = ""
+
+
+def btn_pressed(num: str):
+    global user_count
+    user_count += num
 
 
 def check_position() -> bool:
     global move_stop
     if canvas_animal.coords(canvas_image)[0] <= -250:
+        # playsound(SHEEP_SFX, False)
+        play_sfx()
         canvas_animal.move(canvas_image, 1000, 0)
         return True
     elif canvas_animal.coords(canvas_image)[0] == 250 and move_stop is False:
         move_stop = True
-        playsound(SFX)
         return True
     elif canvas_animal.coords(canvas_image)[0] == 250 and move_stop:
         move_stop = False
@@ -35,13 +56,23 @@ def check_position() -> bool:
         return True
 
 
-def move_image():
+def play_sfx() -> None:
+    chance = random.randint(1, 100)
+    print(chance)
+    if chance <= 25:
+        print("Playing yawn")
+        playsound(random.choice(YAWN_SFX), False)
+    elif chance <= 50:
+        print("Playing sheep")
+        playsound(SHEEP_SFX, False)
+
+
+def move_image() -> None:
     global timer
     if check_position():
         # true: timer start and move image
         canvas_animal.move(canvas_image, -10, 0)
-        print(canvas_animal.coords(canvas_image))
-        timer = root.after(60, func=move_image)
+        timer = root.after(40, func=move_image)
     else:
         # false: timer stop and no more image moving
         root.after_cancel(timer)
@@ -69,11 +100,12 @@ btn_text = 0
 for i in range(3):
     for j in range(3):
         btn_text += 1
-        new_button = GuiButton(master=frame_numpad, text=str(btn_text), row=i, column=j, action=hallo_welt)
+        new_button = GuiButton(master=frame_numpad, text=str(btn_text), row=i, column=j,
+                               action=lambda num=btn_text: btn_pressed(str(num)))
 
-button_zero = GuiButton(master=frame_numpad, text="0", row=3, column=1, action=move_image)
-button_clear = GuiButton(master=frame_numpad, text="Clear", row=3, column=0, action=hallo_welt)
-button_send = GuiButton(master=frame_numpad, text="Send", row=3, column=2, action=hallo_welt)
+button_zero = GuiButton(master=frame_numpad, text="0", row=3, column=1, action=lambda: btn_pressed(str(0)))
+button_clear = GuiButton(master=frame_numpad, text="Clear", row=3, column=0, action=btn_clear)
+button_send = GuiButton(master=frame_numpad, text="Send", row=3, column=2, action=btn_send)
 
 
 root.mainloop()
